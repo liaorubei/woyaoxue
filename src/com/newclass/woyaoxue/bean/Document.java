ed9aa101ee80b33b9c49cbb0bfdc23c1e42239e2
liaorubei@outlook.com <liaorubei@outlook.com>
2015-09-10 15:36:41 +0800

    -

--- src/com/newclass/woyaoxue/bean/Document.java
+++ src/com/newclass/woyaoxue/bean/Document.java
@@ -4,22 +4,15 @@ import java.util.List;
 
 public class Document
 {
-	public Document(int id, String title, String soundPath)
-	{
-		this.Id = id;
-		this.Title = title;
-		this.SoundPath = soundPath;
-	}
-
 	public int Id;
 	public String Title;
 	public List<Lyric> Lyrics;
 	public String SoundPath;
 	public double Duration;
 	public long Length;
-	
-	//是否需要下载
+
+	// 是否需要下载
 	public boolean NeedDownLoad;
-	//声音文件是否已经下载,并已经存在
+	// 声音文件是否已经下载,并已经存在
 	public boolean SoundFileExists;
 }
