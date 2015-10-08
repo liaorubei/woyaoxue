55bc2c9ad6e7b484306300469ec7d13700f90a05
lenovopc <none>
2015-10-08 10:51:21 +0800

    -添加了一个字段

--- src/com/newclass/woyaoxue/bean/Folder.java
+++ src/com/newclass/woyaoxue/bean/Folder.java
@@ -5,11 +5,12 @@ public class Folder
 //{"Id":6,"Name":"舌尖上的中国","DocsCount":0}
 	public int Id;
 	public String Name;
+	public int LevelId;
 	public int DocsCount;
 	@Override
 	public String toString()
 	{
-		return "Folder [Id=" + Id + ", Name=" + Name + ", DocsCount=" + DocsCount + "]";
+		return "Folder [Id=" + Id + ", Name=" + Name + ", LevelId=" + LevelId +", DocsCount=" + DocsCount + "]";
 	}
 	
 }
