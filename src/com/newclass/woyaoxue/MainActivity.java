6f9e96f7398d9af7f3e4e9771ea30cb15dd37512
lenovopc <none>
2015-10-20 12:12:46 +0800

    -

--- src/com/newclass/woyaoxue/MainActivity.java
+++ src/com/newclass/woyaoxue/MainActivity.java
@@ -1,70 +1,17 @@
 package com.newclass.woyaoxue;
 
-import java.util.List;
-
+import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.FragmentActivity;
-import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.PopupWindow;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 
-import com.lidroid.xutils.view.annotation.ViewInject;
-import com.newclass.woyaoxue.activity.*;
-import com.newclass.woyaoxue.bean.Level;
-import com.newclass.woyaoxue.fragment.DocsListFragment;
+import com.newclass.woyaoxue.activity.HomeActivity;
 import com.newclass.woyaoxue.service.BatchDownloadService;
 import com.newclass.woyaoxue.service.DownloadService;
 import com.voc.woyaoxue.R;
 
-public class MainActivity extends FragmentActivity implements android.view.View.OnClickListener
+public class MainActivity extends Activity
 {
 	// #3498db #95a5a6
-
-	protected static final int INIT_LEVEL = 0;
-
-	@ViewInject(R.id.fl_content)
-	private FrameLayout fl_content;
-	@ViewInject(R.id.rg_levels)
-	private RadioGroup rg_levels;
-
-	@ViewInject(R.id.bt_menu)
-	private View bt_menu;
-
-	List<DocsListFragment> fragments;
-	private PackageManager packageManager;
-	@ViewInject(R.id.ll_ctrl)
-	private LinearLayout ll_ctrl;
-
-	private static Handler handler = new Handler()
-	{
-		public void handleMessage(android.os.Message msg)
-		{
-			switch (msg.what)
-			{
-			case INIT_LEVEL:
-				init_level();
-				break;
-
-			default:
-				break;
-			}
-
-		};
-	};
-	private List<Level> levels;
-
-	private PopupWindow window;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -85,78 +32,4 @@ public class MainActivity extends FragmentActivity implements android.view.View.
 		this.finish();
 	}
 
-	protected static void init_level()
-	{
-
-	}
-
-	@Override
-	protected void onDestroy()
-	{
-		super.onDestroy();
-		handler.removeCallbacksAndMessages(null);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main_activity, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-		switch (item.getItemId())
-		{
-		case R.id.menu_mydownload:
-			Intent intent = new Intent(this, DownFolderActivity.class);
-			startActivity(intent);
-			return true;
-
-		default:
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public void onClick(View v)
-	{
-		switch (v.getId())
-		{
-		case R.id.bt_menu:
-			if (window == null)
-			{
-				window = new PopupWindow(this);
-				// window.dismiss();
-				window.setFocusable(true);// 要求可以取得焦点,当失去焦点时可以自动dismiss
-				TextView textView = new TextView(this);
-				textView.setText("我的下载");
-				textView.setOnClickListener(new View.OnClickListener()
-				{
-
-					@Override
-					public void onClick(View v)
-					{
-						window.dismiss();
-						Intent intent = new Intent(MainActivity.this, DownFolderActivity.class);
-						startActivity(intent);
-					}
-
-				});
-				window.setContentView(textView);
-				window.setWindowLayoutMode(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-			}
-
-			window.showAtLocation(bt_menu, Gravity.BOTTOM | Gravity.END, 0, 0);
-
-			break;
-
-		default:
-			break;
-		}
-
-	}
 }
--- src/com/newclass/woyaoxue/activity/HomeActivity.java
+++ src/com/newclass/woyaoxue/activity/HomeActivity.java
@@ -14,11 +14,16 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,27 +34,26 @@ import com.lidroid.xutils.exception.HttpException;
 import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.http.callback.RequestCallBack;
 import com.lidroid.xutils.http.client.HttpRequest.HttpMethod;
-import com.newclass.woyaoxue.base.BaseFragment;
+import com.newclass.woyaoxue.base.BaseAdapter;
+import com.newclass.woyaoxue.bean.Folder;
 import com.newclass.woyaoxue.bean.Level;
 import com.newclass.woyaoxue.bean.database.Database;
 import com.newclass.woyaoxue.bean.database.UrlCache;
-import com.newclass.woyaoxue.fragment.FolderFragment;
 import com.newclass.woyaoxue.service.AutoUpdateService;
 import com.newclass.woyaoxue.util.ConstantsUtil;
 import com.newclass.woyaoxue.util.Log;
 import com.newclass.woyaoxue.util.NetworkUtil;
+import com.newclass.woyaoxue.view.ContentView;
+import com.newclass.woyaoxue.view.ContentView.ViewState;
 import com.voc.woyaoxue.R;
 
 public class HomeActivity extends FragmentActivity
 {
-	private MyFragmentPagerAdapter adapter;
-
-	private List<BaseFragment> fragments;
+	private Database database;
+	private List<Fragment> fragments;
 	private LinearLayout ll_levels;
-
+	private MyFragmentPagerAdapter pagerAdapter;
 	private ViewPager vp_folder;
-
-	protected Database database;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
@@ -76,67 +80,61 @@ public class HomeActivity extends FragmentActivity
 		return super.onOptionsItemSelected(item);
 	}
 
-	@Override
-	protected void onDestroy()
+	/**
+	 * @param json
+	 */
+	private void fillData(List<Level> json)
 	{
-
-		super.onDestroy();
-		database.closeConnection();
-	}
-
-	@SuppressWarnings("deprecation")
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
-		ll_levels = (LinearLayout) findViewById(R.id.ll_levels);
-		vp_folder = (ViewPager) findViewById(R.id.vp_folder);
-
-		fragments = new ArrayList<BaseFragment>();
-		adapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
-
-		vp_folder.setAdapter(adapter);
-		vp_folder.setOnPageChangeListener(new OnPageChangeListener()
+		// 排序
+		Collections.sort(json, new Comparator<Level>()
 		{
 
 			@Override
-			public void onPageScrolled(int arg0, float arg1, int arg2)
+			public int compare(Level lhs, Level rhs)
 			{
-
-			}
-
-			@Override
-			public void onPageScrollStateChanged(int arg0)
-			{
-
-			}
-
-			@Override
-			public void onPageSelected(int arg0)
-			{
-				for (int i = 0; i < ll_levels.getChildCount(); i++)
-				{
-					TextView childAt = (TextView) ll_levels.getChildAt(i);
-					childAt.setTextColor(i == arg0 ? ConstantsUtil.ColorOne : ConstantsUtil.ColorTwo);
-				}
+				return Integer.valueOf(rhs.Sort).compareTo(lhs.Sort);
 			}
 		});
-		database = new Database(HomeActivity.this);
-		loadData();
 
-		// ActionBar
-		getActionBar().setDisplayShowHomeEnabled(true);
+		// ViewPager数据源
+		fragments.clear();
+		for (Level level : json)
+		{
+			fragments.add(new MyFragment(level.Id));
+		}
+		pagerAdapter.notifyDataSetChanged();
 
-		// 自动升级服务
-		Intent service = new Intent(this, AutoUpdateService.class);
-		startService(service);
+		// tabs数据源
+		ll_levels.removeAllViews();
+		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
+		for (int i = 0; i < json.size(); i++)
+		{
+			final int item = i;
+			TextView child = new TextView(HomeActivity.this);
+			child.setGravity(Gravity.CENTER);
+			child.setText(json.get(i).Name);
+			child.setBackgroundResource(R.drawable.selector_levels);
+			child.setTextColor(i == 0 ? ConstantsUtil.ColorOne : ConstantsUtil.ColorTwo);
+
+			child.setOnClickListener(new OnClickListener()
+			{
+
+				@Override
+				public void onClick(View v)
+				{
+					vp_folder.setCurrentItem(item);
+				}
+			});
+
+			ll_levels.addView(child, params);
+		}
+
 	}
 
 	private void loadData()
 	{
-		String requestUrl = NetworkUtil.getLevels();
-		UrlCache cache = database.cacheSelectByUrl(requestUrl);
+		String url = NetworkUtil.getLevels();
+		UrlCache cache = database.cacheSelectByUrl(url);
 		if (cache == null || (System.currentTimeMillis() - cache.UpdateAt > 600000))
 		{
 			Log.i("使用网络数据");
@@ -192,55 +190,187 @@ public class HomeActivity extends FragmentActivity
 
 	}
 
-	/**
-	 * @param json
-	 */
-	private void fillData(List<Level> json)
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
 	{
-		// 排序
-		Collections.sort(json, new Comparator<Level>()
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+		ll_levels = (LinearLayout) findViewById(R.id.ll_levels);
+		vp_folder = (ViewPager) findViewById(R.id.vp_folder);
+
+		fragments = new ArrayList<Fragment>();
+		pagerAdapter = new MyFragmentPagerAdapter(getSupportFragmentManager());
+
+		vp_folder.setAdapter(pagerAdapter);
+		vp_folder.setOnPageChangeListener(new OnPageChangeListener()
 		{
 
 			@Override
-			public int compare(Level lhs, Level rhs)
+			public void onPageScrolled(int arg0, float arg1, int arg2)
 			{
-				return Integer.valueOf(rhs.Sort).compareTo(lhs.Sort);
+
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0)
+			{
+
+			}
+
+			@Override
+			public void onPageSelected(int arg0)
+			{
+				for (int i = 0; i < ll_levels.getChildCount(); i++)
+				{
+					TextView childAt = (TextView) ll_levels.getChildAt(i);
+					childAt.setTextColor(i == arg0 ? ConstantsUtil.ColorOne : ConstantsUtil.ColorTwo);
+				}
 			}
 		});
+		database = new Database(HomeActivity.this);
+		loadData();
 
-		// ViewPager数据源
-		fragments.clear();
-		for (Level level : json)
+		// ActionBar
+		getActionBar().setDisplayShowHomeEnabled(true);
+
+		// 自动升级服务
+		Intent service = new Intent(this, AutoUpdateService.class);
+		startService(service);
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+		database.closeConnection();
+	}
+
+	private class MyAdapter extends BaseAdapter<Folder>
+	{
+
+		public MyAdapter(List<Folder> list)
 		{
-			fragments.add(new FolderFragment(level.Id));
+			super(list);
 		}
-		adapter.notifyDataSetChanged();
 
-		// tabs数据源
-		ll_levels.removeAllViews();
-		LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT, 1);
-		for (int i = 0; i < json.size(); i++)
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent)
 		{
-			final int item = i;
-			TextView child = new TextView(HomeActivity.this);
-			child.setGravity(Gravity.CENTER);
-			child.setText(json.get(i).Name);
-			child.setBackgroundResource(R.drawable.selector_levels);
-			child.setTextColor(i == 0 ? ConstantsUtil.ColorOne : ConstantsUtil.ColorTwo);
+			Folder item = getItem(position);
+			if (convertView == null)
+			{
+				convertView = View.inflate(HomeActivity.this, R.layout.listitem_folder, null);
+				ViewHolder holder = new ViewHolder();
+				holder.tv_folder = (TextView) convertView.findViewById(R.id.tv_folder);
+				holder.tv_counts = (TextView) convertView.findViewById(R.id.tv_counts);
+				convertView.setTag(holder);
+			}
+			ViewHolder holder = (ViewHolder) convertView.getTag();
+			holder.tv_folder.setText(item.Name);
+			holder.tv_counts.setText("课程:" + item.DocsCount);
+			return convertView;
+		}
+	}
 
-			child.setOnClickListener(new OnClickListener()
+	private class MyFragment extends Fragment
+	{
+		private MyAdapter adapter;
+		private ContentView contentView;
+		private List<Folder> list;
+		private ListView listview;
+		private int mLevelId;
+
+		public MyFragment(int id)
+		{
+			this.mLevelId = id;
+			contentView = new ContentView(HomeActivity.this)
 			{
 
 				@Override
-				public void onClick(View v)
+				public View onCreateSuccessView()
 				{
-					vp_folder.setCurrentItem(item);
-				}
-			});
+					View view = View.inflate(HomeActivity.this, R.layout.fragment_folder, null);
+					listview = (ListView) view.findViewById(R.id.listview);
+					list = new ArrayList<Folder>();
+					adapter = new MyAdapter(list);
+					listview.setAdapter(adapter);
+					listview.setOnItemClickListener(new OnItemClickListener()
+					{
 
-			ll_levels.addView(child, params);
+						@Override
+						public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+						{
+							Folder folder = list.get(position);
+
+							Intent intent = new Intent(HomeActivity.this, DocsActivity.class);
+							intent.putExtra("FolderId", folder.Id);
+							intent.putExtra("FolderName", folder.Name);
+							startActivity(intent);
+						}
+					});
+					return view;
+				}
+			};
+
 		}
 
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			String url = NetworkUtil.getFolders(mLevelId);
+			UrlCache cache = database.cacheSelectByUrl(url);
+			if (cache == null || (System.currentTimeMillis() - cache.UpdateAt > 600000))
+			{
+				Log.i("请求网络:" + url);
+				new HttpUtils().send(HttpMethod.GET, url, new RequestCallBack<String>()
+				{
+
+					@Override
+					public void onFailure(HttpException error, String msg)
+					{
+						contentView.showView(ViewState.FAILURE);
+					}
+
+					@Override
+					public void onSuccess(ResponseInfo<String> responseInfo)
+					{
+						// 填充数据
+						subFillData(responseInfo.result);
+						// 缓存数据
+						UrlCache urlCache = new UrlCache();
+						urlCache.Url = this.getRequestUrl();
+						urlCache.Json = responseInfo.result;
+						urlCache.UpdateAt = System.currentTimeMillis();
+						database.cacheInsertOrUpdate(urlCache);
+					}
+				});
+			}
+			else
+			{
+				Log.i("使用缓存:" + url);
+				subFillData(cache.Json);
+			}
+
+			return contentView;
+		}
+
+		private void subFillData(String json)
+		{
+			List<Folder> folders = new Gson().fromJson(json, new TypeToken<List<Folder>>()
+			{}.getType());
+			if (folders.size() > 0)
+			{
+				list.clear();// 因为是在onCreateView中显示数据,有可能会显示多次,然后数据会叠加重复,所以要清除之前的数据
+				list.addAll(folders);
+				contentView.showView(ViewState.SUCCESS);
+			}
+			else
+			{
+				contentView.showView(ViewState.EMPTY);
+			}
+			adapter.notifyDataSetChanged();
+		}
 	}
 
 	private class MyFragmentPagerAdapter extends FragmentPagerAdapter
@@ -258,11 +388,16 @@ public class HomeActivity extends FragmentActivity
 		}
 
 		@Override
-		public Fragment getItem(int arg0)
+		public Fragment getItem(int position)
 		{
-			BaseFragment baseFragment = fragments.get(arg0);
-			baseFragment.initData(HomeActivity.this);
-			return baseFragment;
+			return fragments.get(position);
 		}
 	}
+
+	private class ViewHolder
+	{
+		public TextView tv_counts;
+		public TextView tv_folder;
+	}
+
 }
