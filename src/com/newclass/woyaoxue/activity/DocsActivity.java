b90568b7267befce3ce985b17cbbca480aed8cf3
lenovopc <none>
2015-10-28 14:22:57 +0800

    -

--- src/com/newclass/woyaoxue/activity/DocsActivity.java
+++ src/com/newclass/woyaoxue/activity/DocsActivity.java
@@ -164,41 +164,6 @@ public class DocsActivity extends Activity
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
 		loadData();
-		
-		
-		
-		
-		
-		Builder builder = new AlertDialog.Builder(this);
-		//填充并找出控件
-		View view = View.inflate(this, 1, null);
-		Button btOk = (Button) view.findViewById(R.id.bt_menu);
-		Button bt_cancel=(Button) view.findViewById(R.id.bt_menu);
-		
-		//给控件添加监听		
-		btOk.setOnClickListener(new OnClickListener()
-		{
-			
-			@Override
-			public void onClick(View v)
-			{
-				//处理代码
-			}
-		});
-		
-		bt_cancel.setOnClickListener(new OnClickListener()
-		{
-			
-			@Override
-			public void onClick(View v)
-			{
-		//处理代码	
-			}
-		});
-		
-		builder.setView(view);//自定义dialog最重要的方法
-		AlertDialog alertDialog = builder.create();//创建
-		alertDialog.show();//显示
 	}
 
 	@Override
